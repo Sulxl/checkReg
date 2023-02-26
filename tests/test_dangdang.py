7a266acff183d19437d0a7f5f1947aad1fa2fb72
Sulxl <104665762+Sulxl@users.noreply.github.com>
2023-02-26 22:33:04 +0400

    Update test_dangdang.py

--- tests/test_dangdang.py
+++ tests/test_dangdang.py
@@ -5,8 +5,10 @@ from src.checkreg import dangdang
 
 class Test(TestCase):
     def test_check_phone(self):
+        phone = '971547725729'
         result = dangdang.check_phone(phone)
         self.assertEqual(result['if_exist'], False)
+    
 
 
 if __name__ == '__main__':
